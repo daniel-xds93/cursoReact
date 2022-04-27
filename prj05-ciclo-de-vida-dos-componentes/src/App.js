f3f74dc51fed6465a3231ab47d7de15c32a7b627
Daniel <daniel.xds@hotmail.com>
2022-04-27 15:49:36 -0300

    recebendo o valor da hora em tempo real

--- prj05-ciclo-de-vida-dos-componentes/src/App.js
+++ prj05-ciclo-de-vida-dos-componentes/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      hora: "15:43:00"
+    };
+  }
+
+  // função que indica o que será feito depois que o component é montado
+  componentDidMount(){
+    // a linha abaixo indica que vamos começar um novo time
+    setInterval(()=>{
+      this.setState({hora: new Date().toLocaleTimeString()})
+    }, 1000);
+  }
+
+  render(){
+    return(
+      <div>
+        Horario atual: {this.state.hora} 
+      </div>
+    );
+  }
 }
 
 export default App;
