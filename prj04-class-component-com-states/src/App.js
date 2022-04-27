41cc3eef31c82933985eea0be73b71ca050462ab
Daniel <daniel.xds@hotmail.com>
2022-04-27 11:03:33 -0300

    criação do arquivo de state, e fazendo com que haja alterações conforme o botão seja clicado

---
+++ prj04-class-component-com-states/src/App.js
@@ -0,0 +1,44 @@
+import React, {Component} from "react";
+
+class App extends Component{
+  
+  constructor(props){
+    super(props);
+
+    this.state = {
+      nome: 'Daniel',
+      contador:0
+    }
+
+    // a linha abaixo permite a função add fazer enxergar apropriedade
+    this.add = this.add.bind(this);
+  }
+
+  add(){
+    let state = this.state;
+    state.contador +=1;
+    // a linha abaixo altera o estado do contador
+    // this.setState({contador: 1}) -> aqui o valor fica fixo
+    this.setState(state);
+  }
+
+  subtrair(){
+    console.log("subitraiu...");
+  }
+  
+  render(){
+    return(
+      <div>
+        {this.state.nome}
+
+        <h3>
+          <button onClick={this.subtrair}>-</button>
+           {this.state.contador} 
+          <button onClick={this.add}>+</button>
+        </h3>
+      </div>
+    )
+  }
+}
+
+export default App;
