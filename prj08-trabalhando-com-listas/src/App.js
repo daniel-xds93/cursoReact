15f7f08e965b260b53045bcd09125801b56b51db
Daniel <daniel.xds@hotmail.com>
2022-04-28 19:29:43 -0300

    criando uma lista

--- prj08-trabalhando-com-listas/src/App.js
+++ prj08-trabalhando-com-listas/src/App.js
@@ -2,6 +2,18 @@ import React, { Component } from "react";
 
 class App extends Component() {
   
+  constructor(props){
+    super(props);
+
+    this.state = {
+      feedd:[
+        { id: 1, username: "daniel.xds93", curtidas: 80, comentarios: 14 },
+        { id: 2, username: "aprendendo.doinicio", curtidas: 210, comentarios: 143 },
+        { id: 3, username: "vilma.nunes104", curtidas: 8, comentarios: 6 }
+      ]
+    };
+  }
+
   render(){
     return (
       <div>
