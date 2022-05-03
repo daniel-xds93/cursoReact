c6cd349589f322345ead12a5d39ef8c0f1ae82b7
Daniel <daniel.xds@hotmail.com>
2022-05-03 10:37:15 -0300

    criação concluida do projeto gerando mensagens aleatorias

--- prj12-biscoito-da-sorte/src/App.js
+++ prj12-biscoito-da-sorte/src/App.js
@@ -5,22 +5,41 @@ class App extends Component {
 
   constructor(props){
     super(props);
-    this.state = {}
+    this.state = {
+      textoFrase: ''
+    }
 
-    this.frases = ['Siga os bons e aprenda com eles.', 'O bom-senso vale mais do que muito conhecimento.', 
- 'O riso é a menor distância entre duas pessoas.', 
- 'Deixe de lado as preocupações e seja feliz.',
- 'Realize o óbvio, pense no improvável e conquiste o impossível.',
- 'Acredite em milagres, mas não dependa deles.',
- 'A maior barreira para o sucesso é o medo do fracasso.'];
+    this.abrirMsg = this.abrirMsg.bind(this);
+
+    this.frases = [
+      'Siga os bons e aprenda com eles.', 
+      'O bom-senso vale mais do que muito conhecimento.', 
+      'O riso é a menor distância entre duas pessoas.', 
+      'Deixe de lado as preocupações e seja feliz.',
+      'Realize o óbvio, pense no improvável e conquiste o impossível.',
+      'Acredite em milagres, mas não dependa deles.',
+      'A maior barreira para o sucesso é o medo do fracasso.'
+    ];
+  }
+
+  abrirMsg(){
+    let state = this.state;
+
+    // a linha abaixo gera números aleatórios
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+
+    state.textoFrase = '"'+this.frases[numeroAleatorio] +'"';
+
+    this.setState(state)
+
   }
 
   render(){
     return (
       <div className="container">
         <img src={require('./assets/biscoito.png')} className="img"/>
-        <Botao/>
-        <h3 className="textoFrase">Frase numero 1 aleatória</h3>
+        <Botao nome="Gerar Mensagem" acaoBtb={this.abrirMsg}/>
+        <h3 className="textoFrase">{this.state.textoFrase}</h3>
       </div>
     );
   }
@@ -30,7 +49,7 @@ class Botao extends Component{
   render(){
     return(
       <div>
-        <button>Abri Mensagem</button>
+        <button onClick={this.props.acaoBtb}>{this.props.nome}</button>
       </div>
     );
   }
