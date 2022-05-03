bee2b205d70f2b53f491772f73838c4a294bfd50
Daniel <daniel.xds@hotmail.com>
2022-05-03 12:38:05 -0300

    botao de iniciar cronometro funcionando

--- prj13-cronometro/src/App.js
+++ prj13-cronometro/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from "react";
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      numero: 0
+    }
+
+    this.timer = null;
+
+    this.iniciar = this.iniciar.bind(this);
+    this.parar = this.parar.bind(this);
+
+  }
+
+  iniciar(){
+    this.timer = setInterval(()=>{
+      let state = this.state;
+      state.numero += 0.1;
+      this.setState(state); 
+    },100);
+  }
+
+  parar(){
+
+  }
+
+  render(){
+    return(
+      <div className="container">
+        <img src={require('./assets/cronometro.png')} className="img"/>
+
+        <a className="timer">{this.state.numero.toFixed(1)}</a>
+        <div className="areaBtn">
+          <a className="botao" onClick={this.iniciar}>Iniciar</a>
+          <a className="botao" onClick={this.parar}>Parar</a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
